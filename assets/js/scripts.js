c4c287652d9ba025fcaecd2bb6d74e7bfa1c9987
Richard Gozlan <rickgoz@hotmail.com>
2013-11-17 23:20:14 +0100

    Edit FB & Twitter popup for mobile purpose - Delete unnecessary JS (sendmail)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -42,37 +42,6 @@ jQuery(document).ready(function() {
     $('.social a.pinterest').tooltip();
     $('.social a.flickr').tooltip();
 
-    /*
-        Subscription form
     
-    $('.success-message').hide();
-    $('.error-message').hide();
-
-    $('.subscribe form').submit(function() {
-        var postdata = $('.subscribe form').serialize();
-        $.ajax({
-            type: 'POST',
-            url: 'assets/sendmail.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-                if(json.valid == 0) {
-                    $('.success-message').hide();
-                    $('.error-message').hide();
-                    $('.error-message').html(json.message);
-                    $('.error-message').fadeIn();
-                }
-                else {
-                    $('.error-message').hide();
-                    $('.success-message').hide();
-                    $('.subscribe form').hide();
-                    $('.success-message').html(json.message);
-                    $('.success-message').fadeIn();
-                }
-            }
-        });
-        return false;
-    });
-*/
 });
 
